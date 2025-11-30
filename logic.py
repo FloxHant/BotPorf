59fc7be9e6cd328821e2014ed2e44f7a3ed27fb7
FloxHant <buandaniil10@gmail.com>
2025-11-30 19:20:24 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,260 +1,128 @@
-from logic import DB_Manager
-from config import *
-from telebot import TeleBot
-from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
-from telebot import types
+import sqlite3
+from config import DATABASE
 
-bot = TeleBot(TOKEN)
-hideBoard = types.ReplyKeyboardRemove() 
+skills = [ (_,) for _ in (['Python', 'SQL', 'API', 'Telegram'])]
+statuses = [ (_,) for _ in (['На этапе проектирования', 'В процессе разработки', 'Разработан. Готов к использованию.', 'Обновлен', 'Завершен. Не поддерживается'])]
 
-cancel_button = "Отмена 🚫"
-def cansel(message):
-    bot.send_message(message.chat.id, "Чтобы посмотреть команды, используй - /info", reply_markup=hideBoard)
+class DB_Manager:
+    def __init__(self, database):
+        self.database = database
+
+    def create_tables(self):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            conn.execute('''CREATE TABLE projects (
+                            project_id INTEGER PRIMARY KEY,
+                            user_id INTEGER,
+                            project_name TEXT NOT NULL,
+                            description TEXT,
+                            url TEXT,
+                            status_id INTEGER,
+                            FOREIGN KEY(status_id) REFERENCES status(status_id)
+                        )''') 
+            conn.execute('''CREATE TABLE skills (
+                            skill_id INTEGER PRIMARY KEY,
+                            skill_name TEXT
+                        )''')
+            conn.execute('''CREATE TABLE project_skills (
+                            project_id INTEGER,
+                            skill_id INTEGER,
+                            FOREIGN KEY(project_id) REFERENCES projects(project_id),
+                            FOREIGN KEY(skill_id) REFERENCES skills(skill_id)
+                        )''')
+            conn.execute('''CREATE TABLE status (
+                            status_id INTEGER PRIMARY KEY,
+                            status_name TEXT
+                        )''')
+            conn.commit()
+        print("База данных успешно создана.")
+
+    def __executemany(self, sql, data):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            conn.executemany(sql, data)
+            conn.commit()
+
+    def __select_data(self, sql, data = tuple()):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute(sql, data)
+            return cur.fetchall()
+            
+
+    def default_insert(self):
+        sql = 'INSERT OR IGNORE INTO skills (skill_name) values(?)'
+        data = skills
+        self.__executemany(sql, data)
+        sql = 'INSERT OR IGNORE INTO status (status_name) values(?)'
+        data = statuses
+        self.__executemany(sql, data)
+
+
+    def insert_project(self, data):
+        sql = 'INSERT OR IGNORE INTO projects (user_id, project_name, url, status_id) values(?, ?, ?, ?)'
+        self.__executemany(sql, data)
+
+    def insert_skill(self, user_id, project_name, skill):
+        sql = 'SELECT project_id FROM projects WHERE project_name = ? AND user_id = ?'
+        project_id = self.__select_data(sql, (project_name, user_id))[0][0]
+        skill_id = self.__select_data('SELECT skill_id FROM skills WHERE skill_name = ?', (skill,))[0][0]
+        data = [(project_id,skill_id)]
+        sql = 'INSERT OR IGNORE INTO project_skills VALUES (?, ?)'
+        self.__executemany(sql, data)
+
   
-def no_projects(message):
-    bot.send_message(message.chat.id, 'У тебя пока нет проектов!\nМожешь добавить их с помошью команды /new_project')
+    def get_statuses(self):
+        sql='SELECT status_name from status'
+        return self.__select_data(sql)
+        
+    def get_status_id(self, status_name):
+        sql = 'SELECT status_id FROM status WHERE status_name = ?'
+        res = self.__select_data(sql, (status_name,))
+        if res: return res[0][0]
+        else: return None
 
-def gen_inline_markup(rows):
-    markup = InlineKeyboardMarkup()
-    markup.row_width = 1
-    for row in rows:
-        markup.add(InlineKeyboardButton(row, callback_data=row))
-    return markup
+    def get_projects(self, user_id):
+        return self.__select_data(sql='SELECT * FROM projects WHERE user_id = ?', data = (user_id,))
 
-def gen_markup(rows):
-    markup = ReplyKeyboardMarkup(one_time_keyboard=True)
-    markup.row_width = 1
-    for row in rows:
-        markup.add(KeyboardButton(row))
-    markup.add(KeyboardButton(cancel_button))
-    return markup
+    def get_project_id(self, project_name, user_id):
+        return self.__select_data(sql='SELECT project_id FROM projects WHERE project_name = ? AND user_id = ?  ', data = (project_name, user_id,))[0][0]
 
-attributes_of_projects = {'Имя проекта' : ["Введите новое имя проекта", "project_name"],
-                          "Описание" : ["Введите новое описание проекта", "description"],
-                          "Ссылка" : ["Введите новую ссылку на проект", "url"],
-                          "Статус" : ["Выберите новый статус задачи", "status_id"]}
-
-def info_project(message, user_id, project_name):
-    info = manager.get_project_info(user_id, project_name)[0]
-    skills = manager.get_project_skills(project_name)
-    if not skills:
-        skills = 'Навыки пока не добавлены'
-    bot.send_message(message.chat.id, f"""Project name: {info[0]}
-Description: {info[1]}
-Link: {info[2]}
-Status: {info[3]}
-Skills: {skills}
-""")
-
-@bot.message_handler(commands=['start'])
-def start_command(message):
-    bot.send_message(message.chat.id, """Привет! Я бот-менеджер проектов
-Помогу тебе сохранить твои проекты и информацию о них!) 
-""")
-    info(message)
+    def get_skills(self):
+        return self.__select_data(sql='SELECT * FROM skills')
     
-@bot.message_handler(commands=['info'])
-def info(message):
-    bot.send_message(message.chat.id,
+    def get_project_skills(self, project_name):
+        res = self.__select_data(sql='''SELECT skill_name FROM projects 
+JOIN project_skills ON projects.project_id = project_skills.project_id 
+JOIN skills ON skills.skill_id = project_skills.skill_id 
+WHERE project_name = ?''', data = (project_name,) )
+        return ', '.join([x[0] for x in res])
+    
+    def get_project_info(self, user_id, project_name):
+        sql = """
+SELECT project_name, description, url, status_name FROM projects 
+JOIN status ON
+status.status_id = projects.status_id
+WHERE project_name=? AND user_id=?
 """
-Вот команды которые могут тебе помочь:
-
-/new_project - используй для добавления нового проекта
-/skills - выбери проект для которого нужно выбрать навык
-/delete - удаление проекта
-/projects - показывает все проекты
-/update_projects - обновление проекта
-/info -  информация
-Также ты можешь ввести имя проекта и узнать информацию о нем!""")
+        return self.__select_data(sql=sql, data = (project_name, user_id))
     
 
-@bot.message_handler(commands=['new_project'])
-def addtask_command(message):
-    bot.send_message(message.chat.id, "Введите название проекта:")
-    bot.register_next_step_handler(message, name_project)
-
-def name_project(message):
-    name = message.text
-    user_id = message.from_user.id
-    data = [user_id, name]
-    bot.send_message(message.chat.id, "Введите ссылку на проект")
-    bot.register_next_step_handler(message, link_project, data=data)
-
-def link_project(message, data):
-    data.append(message.text)
-    statuses = [x[0] for x in manager.get_statuses()] 
-    bot.send_message(message.chat.id, "Введите текущий статус проекта", reply_markup=gen_markup(statuses))
-    bot.register_next_step_handler(message, callback_project, data=data, statuses=statuses)
-
-def callback_project(message, data, statuses):
-    status = message.text
-    if message.text == cancel_button:
-        cansel(message)
-        return
-    if status not in statuses:
-        bot.send_message(message.chat.id, "Ты выбрал статус не из списка, попробуй еще раз!)", reply_markup=gen_markup(statuses))
-        bot.register_next_step_handler(message, callback_project, data=data, statuses=statuses)
-        return
-    status_id = manager.get_status_id(status)
-    data.append(status_id)
-    manager.insert_project([tuple(data)])
-    bot.send_message(message.chat.id, "Проект сохранен")
+    def update_projects(self, param, data):
+        self.__executemany(f"UPDATE projects SET {param} = ? WHERE project_name = ? AND user_id = ?", [data]) # data ('atr', 'mew', 'name', 'user_id')
 
 
-@bot.message_handler(commands=['skills'])
-def skill_handler(message):
-    user_id = message.from_user.id
-    projects = manager.get_projects(user_id)
-    if projects:
-        projects = [x[2] for x in projects]
-        bot.send_message(message.chat.id, 'Выбери проект для которого нужно выбрать навык', reply_markup=gen_markup(projects))
-        bot.register_next_step_handler(message, skill_project, projects=projects)
-    else:
-        no_projects(message)
+    def delete_project(self, user_id, project_id):
+        sql = "DELETE FROM projects WHERE user_id = ? AND project_id = ? "
+        self.__executemany(sql, [(user_id, project_id)])
+
+    def delete_skill(self, project_id, skill_id):
+        sql = "DELETE FROM skills WHERE skill_id = ? AND project_id = ? "
+        self.__executemany(sql, [(skill_id, project_id)])
 
 
-def skill_project(message, projects):
-    project_name = message.text
-    if message.text == cancel_button:
-        cansel(message)
-        return
-        
-    if project_name not in projects:
-        bot.send_message(message.chat.id, 'У тебя нет такого проекта, попробуй еще раз!) Выбери проект для которого нужно выбрать навык', reply_markup=gen_markup(projects))
-        bot.register_next_step_handler(message, skill_project, projects=projects)
-    else:
-        skills = [x[1] for x in manager.get_skills()]
-        bot.send_message(message.chat.id, 'Выбери навык', reply_markup=gen_markup(skills))
-        bot.register_next_step_handler(message, set_skill, project_name=project_name, skills=skills)
-
-def set_skill(message, project_name, skills):
-    skill = message.text
-    user_id = message.from_user.id
-    if message.text == cancel_button:
-        cansel(message)
-        return
-        
-    if skill not in skills:
-        bot.send_message(message.chat.id, 'Видимо, ты выбрал навык. не из спика, попробуй еще раз!) Выбери навык', reply_markup=gen_markup(skills))
-        bot.register_next_step_handler(message, set_skill, project_name=project_name, skills=skills)
-        return
-    manager.insert_skill(user_id, project_name, skill )
-    bot.send_message(message.chat.id, f'Навык {skill} добавлен проекту {project_name}')
-
-
-@bot.message_handler(commands=['projects'])
-def get_projects(message):
-    user_id = message.from_user.id
-    projects = manager.get_projects(user_id)
-    if projects:
-        text = "\n".join([f"Project name:{x[2]} \nLink:{x[4]}\n" for x in projects])
-        bot.send_message(message.chat.id, text, reply_markup=gen_inline_markup([x[2] for x in projects]))
-    else:
-        no_projects(message)
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-    project_name = call.data
-    info_project(call.message, call.from_user.id, project_name)
-
-
-@bot.message_handler(commands=['delete'])
-def delete_handler(message):
-    user_id = message.from_user.id
-    projects = manager.get_projects(user_id)
-    if projects:
-        text = "\n".join([f"Project name:{x[2]} \nLink:{x[4]}\n" for x in projects])
-        projects = [x[2] for x in projects]
-        bot.send_message(message.chat.id, text, reply_markup=gen_markup(projects))
-        bot.register_next_step_handler(message, delete_project, projects=projects)
-    else:
-        no_projects(message)
-
-def delete_project(message, projects):
-    project = message.text
-    user_id = message.from_user.id
-
-    if message.text == cancel_button:
-        cansel(message)
-        return
-    if project not in projects:
-        bot.send_message(message.chat.id, 'У тебя нет такого проекта, попробуй выбрать еще раз!', reply_markup=gen_markup(projects))
-        bot.register_next_step_handler(message, delete_project, projects=projects)
-        return
-    project_id = manager.get_project_id(project, user_id)
-    manager.delete_project(user_id, project_id)
-    bot.send_message(message.chat.id, f'Проект {project} удален!')
-
-
-@bot.message_handler(commands=['update_projects'])
-def update_project(message):
-    user_id = message.from_user.id
-    projects = manager.get_projects(user_id)
-    if projects:
-        projects = [x[2] for x in projects]
-        bot.send_message(message.chat.id, "Выбери проект, который хочешь изменить", reply_markup=gen_markup(projects))
-        bot.register_next_step_handler(message, update_project_step_2, projects=projects )
-    else:
-        no_projects(message)
-
-def update_project_step_2(message, projects):
-    project_name = message.text
-    if message.text == cancel_button:
-        cansel(message)
-        return
-    if project_name not in projects:
-        bot.send_message(message.chat.id, "Что-то пошло не так!) Выбери проект, который хочешь изменить еще раз:", reply_markup=gen_markup(projects))
-        bot.register_next_step_handler(message, update_project_step_2, projects=projects )
-        return
-    bot.send_message(message.chat.id, "Выбери, что требуется изменить в проекте", reply_markup=gen_markup(attributes_of_projects.keys()))
-    bot.register_next_step_handler(message, update_project_step_3, project_name=project_name)
-
-def update_project_step_3(message, project_name):
-    attribute = message.text
-    reply_markup = None 
-    if message.text == cancel_button:
-        cansel(message)
-        return
-    if attribute not in attributes_of_projects.keys():
-        bot.send_message(message.chat.id, "Кажется, ты ошибся, попробуй еще раз!)", reply_markup=gen_markup(attributes_of_projects.keys()))
-        bot.register_next_step_handler(message, update_project_step_3, project_name=project_name)
-        return
-    elif attribute == "Статус":
-        rows = manager.get_statuses()
-        reply_markup=gen_markup([x[0] for x in rows])
-    bot.send_message(message.chat.id, attributes_of_projects[attribute][0], reply_markup = reply_markup)
-    bot.register_next_step_handler(message, update_project_step_4, project_name=project_name, attribute=attributes_of_projects[attribute][1])
-
-def update_project_step_4(message, project_name, attribute): 
-    update_info = message.text
-    if attribute== "status_id":
-        rows = manager.get_statuses()
-        if update_info in [x[0] for x in rows]:
-            update_info = manager.get_status_id(update_info)
-        elif update_info == cancel_button:
-            cansel(message)
-        else:
-            bot.send_message(message.chat.id, "Был выбран неверный статус, попробуй еще раз!)", reply_markup=gen_markup([x[0] for x in rows]))
-            bot.register_next_step_handler(message, update_project_step_4, project_name=project_name, attribute=attribute)
-            return
-    user_id = message.from_user.id
-    data = (update_info, project_name, user_id)
-    manager.update_projects(attribute, data)
-    bot.send_message(message.chat.id, "Готово! Обновления внесены!)")
-
-
-@bot.message_handler(func=lambda message: True)
-def text_handler(message):
-    user_id = message.from_user.id
-    projects =[ x[2] for x in manager.get_projects(user_id)]
-    project = message.text
-    if project in projects:
-        info_project(message, user_id, project)
-        return
-    bot.reply_to(message, "Тебе нужна помощь?")
-    info(message)
-
-    
 if __name__ == '__main__':
     manager = DB_Manager(DATABASE)
-    bot.infinity_polling()
+    manager.create_tables()
+    manager.default_insert()
